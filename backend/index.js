392b80f201694825e973d68fbcb64623c92ce0cf
nika <nikaonime@gmail.com>
2024-06-25 15:54:46 +0300

    create token via jwt

--- backend/index.js
+++ backend/index.js
@@ -21,23 +21,45 @@ const app = express();
 app.use(express.json());
 
 app.post('/auth/register', registerValidation, async (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty())
-        return res.status(400).json(errors.array().map((item) => item.msg));
+    try {
+        const errors = validationResult(req);
+        if (!errors.isEmpty())
+            return res.status(400).json(errors.array().map((item) => item.msg));
 
-    const password = req.body.password;
-    const salt = await bcrypt.genSalt(10);
-    const passwordHash = await bcrypt.hash(password, salt);
+        const password = req.body.password;
+        const salt = await bcrypt.genSalt(10);
+        const hash = await bcrypt.hash(password, salt);
 
-    const doc = new UserModel({
-        email: req.body.email,
-        fullName: req.body.fullName,
-        passwordHash,
-    });
+        const doc = new UserModel({
+            email: req.body.email,
+            fullName: req.body.fullName,
+            passwordHash: hash,
+        });
 
-    const user = await doc.save();
+        const user = await doc.save();
 
-    res.json(user);
+        const token = jwt.sign(
+            {
+                _id: user._id,
+            },
+            'secret123',
+            {
+                expiresIn: '365d',
+            }
+        );
+
+        const { passwordHash, ...userData } = user._doc;
+
+        res.json({
+            ...userData,
+            token,
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            message: 'Failed to register',
+        });
+    }
 });
 
 app.listen(2222, (err) => {
