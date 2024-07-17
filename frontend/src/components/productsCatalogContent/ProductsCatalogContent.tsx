1da277d5d679ddd89fe92a93528f6c6d09e58240
nika <nikaonime@gmail.com>
2024-07-17 14:45:21 +0300

    ProductsCatalogContent(add_product styles)

--- frontend/src/components/productsCatalogContent/ProductsCatalogContent.tsx
+++ frontend/src/components/productsCatalogContent/ProductsCatalogContent.tsx
@@ -1,6 +1,9 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 
 import { IProduct } from 'types/product';
+
+import plusIcon from 'assets/icons/plus.png';
+import addIcon from 'assets/icons/add_mark.png';
 
 import Product from './product/Product';
 
@@ -13,8 +16,80 @@ interface ProductsCatalogContentProps {
 const ProductsCatalogContent: FC<ProductsCatalogContentProps> = ({
     products,
 }) => {
+    const [isEdit, setIsEdit] = useState<boolean>(false);
+    const [type, setType] = useState<IProduct['type']>('main');
+
+    const toggleEdit = () => {
+        setIsEdit(!isEdit);
+    };
+
+    const toggleType = (type: IProduct['type']) => {
+        setType(type);
+    };
+
+    const handleAddProduct = () => {
+        toggleEdit();
+        setType('main');
+    };
+
     return (
         <ul className={styles.products}>
+            <li className={styles.add_product}>
+                {isEdit ? (
+                    <div className={styles.edit}>
+                        <div className={styles.inputs}>
+                            <input
+                                className={styles.name}
+                                type="text"
+                                placeholder="Name"
+                            />
+                            <input
+                                className={styles.cost}
+                                type="number"
+                                placeholder="Price"
+                            />
+                        </div>
+                        <div className={styles.edit_info}>
+                            <ul className={styles.type}>
+                                <li
+                                    onClick={() => toggleType('main')}
+                                    className={
+                                        type === 'main' ? styles.active : ''
+                                    }
+                                >
+                                    Main
+                                </li>
+                                <li
+                                    onClick={() => toggleType('dessert')}
+                                    className={
+                                        type === 'dessert' ? styles.active : ''
+                                    }
+                                >
+                                    Dessert
+                                </li>
+                                <li
+                                    onClick={() => toggleType('drinks')}
+                                    className={
+                                        type === 'drinks' ? styles.active : ''
+                                    }
+                                >
+                                    Drinks
+                                </li>
+                            </ul>
+                            <button
+                                onClick={handleAddProduct}
+                                className={styles.edit_add}
+                            >
+                                <img src={addIcon} alt="checkmark" />
+                            </button>
+                        </div>
+                    </div>
+                ) : (
+                    <button onClick={toggleEdit} className={styles.add}>
+                        <img src={plusIcon} alt="plus" />
+                    </button>
+                )}
+            </li>
             {products.map((item) => (
                 <Product key={item._id} product={item} />
             ))}
